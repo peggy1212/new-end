b9cae4631b4cfa4fddaeedcc3fd9695730ceda2d
peggy1212 <kahingpeggy@gmail.com>
2021-07-12 14:20:59 +0800

    add tokens

--- packages/react-app/src/views/Subgraph.jsx
+++ packages/react-app/src/views/Subgraph.jsx
@@ -6,29 +6,35 @@ function Subgraph() {
 const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState({});
-const result = JSON.stringify(items);
 
 
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(() => {
-    fetch("https://api.pancakeswap.info/api/v2/tokens/0xea01a1a3cf143f90b4ac6d069bd369826574cd45")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
+  Promise.all([
+    fetch("https://api.pancakeswap.info/api/v2/tokens/0xea01a1a3cf143f90b4ac6d069bd369826574cd45"),
+    fetch("https://api.pancakeswap.info/api/v2/tokens/0x4C8307cF2cfAFCAb5653c25e1870b905488bD830"),
+    fetch("https://api.pancakeswap.info/api/v2/tokens/0x749E90d966ece8fc5a30Ed150E4361F40dB00219"),
+    fetch("https://api.pancakeswap.info/api/v2/tokens/0x1C2425F50a1e98D5631b223E56760d21BC840C53")
+      ]).then(function (responses) {
+             // Get a JSON object from each of the responses
+             return Promise.all(responses.map(function (response) {
+                 return response.json();
+             }));
+         }).then((result) => {
+                           setIsLoaded(true);
+                           setItems(result);
+                         },
+                         // Note: it's important to handle errors here
+                         // instead of a catch() block so that we don't swallow
+                         // exceptions from actual bugs in components.
+                         (error) => {
+                           setIsLoaded(true);
+                           setError(error);
+                         }
+                       )
+                   }, [])
 
   if (error) {
     return <div>Error: {error.message}</div>;
